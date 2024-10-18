6d666fe3a577e054fc9312aeae87ccbfb10c8eb2
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-10-18 22:35:15 +0200

    the allow rotate when page load

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ for(let i = 1; i<= 60; i++){
   );
 }
 barSeconds.insertAdjacentHTML("afterbegin",barElement.join(""));
+
+
+const handHours = document.querySelector('.hand.hours');
+const handMinutes = document.querySelector('.hand.minutes');
+const handSeconds = document.querySelector('.hand.seconds');
+
+
+function getCurrentTime(){
+  let date = new Date();
+  let currentHours = date.getHours();
+  let currentMinutes = date.getMinutes();
+  let currentSeconds = date.getSeconds();
+
+  handSeconds.style.transform = `rotate(${currentSeconds*6}deg)`;
+
+}
+
+//call getCurrentTime function on page load
+getCurrentTime();
