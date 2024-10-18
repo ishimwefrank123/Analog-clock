708b55d5e66688e544b23d1287efb5256b4fa752
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-10-18 21:09:56 +0200

    add number hour

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ const numberElement = [];
 
 // create the number hours
 for(let i=1; i <= 12; i++){
-  
+  numberElement.push(
+    `<span><p>${i}</p></span>`
+  );
+  console.log(numberElement)
 }
+
